a130ff86dcc0a7f52ca2b16ed16ccb3a2a817d10
Pierre-Alexandre St-Jean <pa@stjean.me>
2014-02-22 12:26:14 -0500

    ASDASDASD

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,25 @@
-var gulp = require('gulp');
-var stylus = require('gulp-stylus');
+var gulp = require('gulp'),
+    stylus = require('gulp-stylus'),
+    watch = require('gulp-watch'),
+    plumber = require('gulp-plumber');
 
 
+// Get and render all .styl files recursively
+gulp.task('stylus', function () {
+    gulp.src('assets/styles/style.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('assets/styles/'));
+});
+
+gulp.task('watch', function(){
+    gulp.src('assets/styles/style.styl')
+    .pipe(watch())
+    .pipe(plumber())
+    .pipe(stylus())
+    .pipe(gulp.dest('assets/styles/'));
+
+
+})
+
+gulp.task('default',function(){
+})
