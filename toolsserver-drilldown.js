145a7873da1e9c94ea6e38592ad415b1780b5c89
Pablo Herrera <pablo.herrera-batz@erpguru.com>
2014-02-22 19:20:46 -0500

    create drilldown architecture

---
+++ toolsserver-drilldown.js
@@ -0,0 +1,62 @@
+var express = require("express"),
+    app = express(),
+    port = parseInt(process.env.PORT,10) || 9000;
+
+var objectToSend = undefined;
+var testIndex = 1;
+
+app.get('/', function(req, res){
+   res.redirect('/drilldown.html');
+});
+
+app.get('/data.json',function(req,res){
+  res.setHeader("Content-Type","application/json");
+  res.write(JSON.stringify(objectToSend || intitializeObject()));
+  res.end()
+})
+
+app.use('/', express.static(__dirname));
+app.listen(port);
+
+var generateThaList = function(currentTime){
+  var list = [];
+  var l = Math.floor(Math.random() * 10);
+
+  for(var i = 0; i < l; i++) {
+    list.push({
+      'id': 'J422' + getPaddedNumber(testIndex),
+      'enteredTime': 1239081237908 + currentTime + Math.floor(Math.random() * 1000 * 60 * 15),
+      'type': [true,false][Math.round(Math.random())] ? 'troponin' : undefined
+    });
+    testIndex++;
+  }
+
+  return list
+}
+
+var intitializeObject = function(){
+  var currentTime = Date.now();
+  o = {}
+  o.metadata = {servertime:currentTime}
+  o.data = generateThaList(currentTime);
+  return o;
+}
+
+function getPaddedNumber(num) {
+  num = num + '';
+  return new Array(4 - num.length + 1).join(0) + num;
+}
+
+setInterval(function(){
+  if (objectToSend) {
+    testIndex++;
+    objectToSend.data.splice(0,1);
+    objectToSend.data.push({
+      'id': 'J422' + getPaddedNumber(testIndex),
+      'enteredTime': 1239081237908 + Date.now() + Math.floor(Math.random() * 1000 * 60 * 15),
+      'type': [true,false][Math.round(Math.random())] ? 'troponin' : null
+    });
+  }
+}, 5000);
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
